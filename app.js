4f57df2082c52604657d74b65f0922e4df7e38b6
shaown <smshaown65@gmail.com>
2021-06-24 07:35:44 +0600

    key up the file setup

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ loadEventListeners();
 function loadEventListeners(){
     // Add Task Event
     form.addEventListener('submit', addTask)
+    // Remove Task
+    taskList.addEventListener('click', removeTask)
+    // Clear Task
+    clearBtn.addEventListener('click', clearTask);
+    // Filter
+    filter.addEventListener('keyup', filterTask);
 }
 
 //function: addTask()
@@ -34,13 +40,56 @@ function addTask(e){
         // Add Class
         link.className = 'delete-item secondary-content';
         // Add Icon HTML
-        link.innerHTML = '<i class="fa fa-remove"></i>';
+        link.innerHTML = '<i class="fa fa-times-circle"></i>';
         // Append link to li
         li.appendChild(link);
 
          
         // Append li to ul <next to link--> i tag create>
         taskList.appendChild(li);
+
+        // Add into Ls
     }
     e.preventDefault();
+}
+
+// Function: removeTask()
+function removeTask(e){
+    
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm('Are You Sure ?')){
+            e.target.parentElement.parentElement.remove();
+            
+            // Remove from ls
+        }
+    }
+    console.log('click event fired')
+    e.preventDefault();
+}
+
+// Function: ClearTask()
+function clearTask(){
+    // taskList.innerHTML = '';
+
+    //Faster Way
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    }
+
+    //Cleat from LS
+}
+
+// Function: FilterTask()
+function filterTask(e){
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+
+        if(item.toLocaleLowerCase().indexOf(text) != -1){
+            task.style.display = 'block';
+        }else{
+            task.style.display = 'none';
+        }
+    })
 }
